d16dbb1d63eaa97897c47102fa75961b0f434215
Zach Gwirtz <zachgw@gmail.com>
2019-10-03 10:05:31 -0700

    added water consumed get/patch route

--- src/hydrate/hydrate-router.js
+++ src/hydrate/hydrate-router.js
@@ -9,7 +9,7 @@ const knexInstance = knex({
     connection: DB_URL,
 });
 
-hydrateRouter
+hydrateRouter   // get all users... do i need this?
     .route('/api/user')
     .get((req, res) => {
         knexInstance
@@ -19,7 +19,7 @@ hydrateRouter
                 res.json(results)
             })
     })
-    .post(jsonParser, (req, res, next) => {
+    .post(jsonParser, (req, res, next) => {  // register new users
         const { username, password, glasses } = req.body;
         const newUser = { username, password, glasses };
         knexInstance
@@ -32,7 +32,7 @@ hydrateRouter
     })
 
 hydrateRouter
-    .route('/api/user/:id')
+    .route('/api/user/:id') // display current user profile
     .get((req, res, next) => {
         const {id} = req.params;
         knexInstance
@@ -44,23 +44,60 @@ hydrateRouter
             })
             .catch(next)
     })
-    .patch(jsonParser, (req, res, next) => {
+    .patch(jsonParser, (req, res, next) => {  // edit water consumption goal
         const { glasses } = req.body
         const glassesUpdate = { glasses }
-        const {id} = req.params
+        const { id } = req.params
 
         const numberOfValues = Object.values(glassesUpdate).filter(Boolean).length
         if (numberOfValues === 0)
-        return res.status(400).json({
-        error: {
-        message: `Request body must contain either 'glasses'`
-        }
-      })
+            return res.status(400).json({
+            error: { message: `Request body must contain 'glasses'` }
+            })
 
         knexInstance('hydrate_users')
             .where( {id} )
             .update({glasses})
             .then(user => {
+                res.status(204).end();
+            })
+            .catch(next)
+    })
+
+hydrateRouter
+    .route('/api/user/login')  // user login 
+    .post((req, res, next) => {
+
+    })
+
+hydrateRouter
+    .route('/api/user/waterconsumed/:user_id')  // display water consumed/day
+    .get((req, res, next) => {
+        const {user_id} = req.params;
+        knexInstance
+            .from('hydrate_quota')
+            .select('*')
+            .where('user_id', user_id)
+            .then(water => {
+                res.json(water)
+            })
+            .catch(next)
+    })
+    .patch(jsonParser, (req, res, next) => { // update water consumed
+        const { amount } = req.body
+        const amountUpdate = { amount }
+        const { id } = req.params
+
+        const numberOfValues = Object.values(amountUpdate).filter(Boolean).length
+        if (numberOfValues === 0)
+            return res.status(400).json({
+            error: { message: `Request body must contain 'amount'` }
+            })
+
+        knexInstance('hydrate_quotas')
+            .where( {id} )
+            .update( {amount} )
+            .then(amount => {
                 res.status(204).end();
             })
             .catch(next)
